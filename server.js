bedc6e9c6d1ee7cc14b965e95b7fdeb04fb2b506
Surya <suryabusiness152@gmail.com>
2026-02-24 08:08:58 +0000

    initial honeypot dashboard project

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
  
 const ADMIN_USER = "admin";
 const ADMIN_PASS = "admin123";
- 
+const failedAttempts ={};
+const MAX_ATTEMPTS =5;
+const BLOCK_TIME = 2*60*1000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -15,7 +18,7 @@ app.use(express.static("public"));
 app.use(session({
     secret: "honeypotsecret",
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false
 }));
  
 // Log everything except dashboard
@@ -41,19 +44,75 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+
+
+
+
+
+
+
+
+//LOGIN HANDLER //
  
-// Login handler
-app.post("/login", (req, res) => {
+   app.post("/login", (req, res) => {
+    const ip = req.ip;
     const { username, password } = req.body;
  
+    // If IP is blocked
+    if (failedAttempts[ip] && failedAttempts[ip].blockedUntil > Date.now()) {
+        return res.send("🚫 Too many attempts. Try later.");
+    }
+ 
     if (username === ADMIN_USER && password === ADMIN_PASS) {
+        failedAttempts[ip] = { count: 0 };
         req.session.loggedIn = true;
         return res.redirect("/dashboard");
+    }
+ 
+    // Wrong login
+    if (!failedAttempts[ip]) {
+        failedAttempts[ip] = { count: 0 };
+    }
+ 
+    failedAttempts[ip].count++;
+ 
+    if (failedAttempts[ip].count >= MAX_ATTEMPTS) {
+        failedAttempts[ip].blockedUntil = Date.now() + BLOCK_TIME;
+        return res.send("🚫 IP Blocked for 2 minutes.");
     }
  
     res.send("❌ Invalid credentials");
 });
  
+
+ 
+
+//logout route//
+app.get("/logout",(req,res)=>{
+req.session.destroy((err)=> {
+if(err){
+console.log(err);
+return res.redirect("/");
+}
+res.clearCookie("connect.sid");
+res.redirect("/");
+});
+});
+
+app.get("/clear-logs", (req, res) => {
+    if (!req.session.loggedIn) {
+        return res.redirect("/");
+    }
+ 
+    fs.writeFileSync("attacks.log", "");
+    res.redirect("/dashboard");
+});
+
+
+
+
+
 // Dashboard (protected)
 app.get("/dashboard", (req, res) => {
  
@@ -63,6 +122,9 @@ app.get("/dashboard", (req, res) => {
  
     res.sendFile(path.join(__dirname, "public", "dashboard.html"));
 });
+
+
+//api logs//
 app.get("/api/logs", (req, res) => {
  
     if (!req.session.loggedIn) {
@@ -79,7 +141,10 @@ app.get("/api/logs", (req, res) => {
         .map(line => JSON.parse(line));
  
     res.json(data);
-}); 
+});
+
+
+
 app.listen(80, () => {
     console.log("Honeypot running on port 80");
 });
